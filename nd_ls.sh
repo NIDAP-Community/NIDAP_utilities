476e4b09e2b0da2e49d037a9c3a08b3bb7ff2182
PalantirContractor <122635305+PalantirContractor@users.noreply.github.com>
2023-01-25 11:03:08 -0500

    Add files via upload

--- nd_ls.sh
+++ nd_ls.sh
@@ -8,9 +8,13 @@
 ##################################################################
 #
 # Usage:
-# sh nd_ls.sh "folder_path_name_to_list" ["-t"]
+# sh nd_ls.sh "folder_path_name_to_list"
 #
-#             -t is optional, tests functionality;
+# sh nd_ls.sh -h
+#             -h flag; displays function help;
+#
+# sh nd_ls.sh -t
+#             -t flag; test function;
 #
 ##################################################################
 #
@@ -18,12 +22,41 @@
 # nothing
 #
 ##################################################################
-#!/bin/bash
 
-# test routine
-if [ "$#" -ge 2 ] && [ "$2" = "-t" ]
+# execute request
+if [ "$#" -eq 1 ] && [ "$1" != "-h" ] && [ "$1" != "-t" ]
 then
-    getTest=$(ls "$1")
+    echo "Listing folder(s): [$1]...";
+    ls -C -al | awk '{ print $6 "/" $7 "/" $8 "\t" $5 "\t" $9}'
+# display help
+elif [ "$#" -eq 1 ] && [ "$1" = "-h" ]
+then
+    echo "##################################################################"
+    echo "#"
+    echo "# About This Script:"
+    echo "# nd_ls: implements the BASH ls command"
+    echo "#"
+    echo "##################################################################"
+    echo "#"
+    echo "# Usage:"
+    echo "# sh nd_ls.sh ""folder_path_name_to_list"""
+    echo "#"
+    echo "# sh nd_ls.sh -h"
+    echo "#             -h flag; displays function help;"
+    echo "#"
+    echo "# sh nd_ls.sh -t"
+    echo "#             -t flag; test function;"
+    echo "#"
+    echo "##################################################################"
+    echo "#"
+    echo "# Returns:"
+    echo "# nothing"
+    echo "#"
+    echo "##################################################################"
+# test routine
+elif [ "$#" -eq 1 ] && [ "$1" = "-t" ]
+then
+    getTest=$(ls ".")
     #echo "$getTest"
     if ! test -z "$getTest"
     then
@@ -31,16 +64,6 @@ then
     else
         echo "nd_ls: ERROR: failed to list folder contents; verify folder pathname and/or permissions."
     fi
-elif [ "$#" -ge 2 ] && [ "$2" != "-t" ]
-then
-    echo "nd_ls: FAIL: unrecognized flag; test syntax is: sh nd_ls.sh ""folder_path_name_to_list"" [""-t""]."
 else
-    # execute request
-    if [ "$#" -ge 1 ]
-    then
-        echo "Listing folder(s): [$1]...";
-        ls -C -al | awk '{ print $6 "/" $7 "/" $8 "\t" $5 "\t" $9}'
-    else
-        echo "nd_ls: ERROR: folder pathname is missing; syntax is: sh nd_ls.sh ""folder_path_name_to_list"""
-    fi
+    echo "nd_ls: ERROR: unrecognized input; syntax is: sh nd_ls.sh -h"
 fi
--- nd_rm.sh
+++ nd_rm.sh
@@ -8,11 +8,15 @@
 ##################################################################
 #
 # Usage:
-# sh nd_rm.sh "file_path_name_to_delete.extension" ["-i"] ["-t"]
-#
+# sh nd_rm.sh "file_path_name_to_delete.extension" [-i]
 #             -i is optional, forces interactive confirmation
 #                prompt;
-#             -t is optional, tests functionality (ignores file
+#
+# sh nd_rm.sh -h
+#             -h flag; displays function help;
+#
+# sh nd_rm.sh -t
+#             -t flg; tests functionality (ignores file
 #                pathname provided; deletes temporary test file);
 #
 ##################################################################
@@ -21,10 +25,48 @@
 # nothing
 #
 ##################################################################
-#!/bin/bash
 
+# execute request
+if [ "$#" -eq 1 ] || [ "$#" -eq 2 ] && [ "$1" != "-h" ] && [ "$1" != "-t" ]
+then
+    echo "Deleting file(s): [$1]...";
+    if [ "$#" -eq 2 ] && [ "$2" = "-i" ]
+    then
+        echo "Using Switch(es): [$2]...";
+        rm -i "$1"
+    else
+        rm "$1"
+    fi
+# display help
+elif [ "$#" -eq 1 ] && [ "$1" = "-h" ]
+then
+    echo "##################################################################"
+    echo "#"
+    echo "# About This Script:"
+    echo "# nd_rm: implements the BASH rm command"
+    echo "#"
+    echo "##################################################################"
+    echo "#"
+    echo "# Usage:"
+    echo "# sh nd_rm.sh ""file_path_name_to_delete.extension"" [-i]"
+    echo "#             -i is optional, forces interactive confirmation"
+    echo "#                prompt;"
+    echo "#"
+    echo "# sh nd_rm.sh -h"
+    echo "#             -h flag; displays function help;"
+    echo "#"
+    echo "# sh nd_rm.sh -t"
+    echo "#             -t flg; tests functionality (ignores file"
+    echo "#                pathname provided; deletes temporary test file);"
+    echo "#"
+    echo "##################################################################"
+    echo "#"
+    echo "# Returns:"
+    echo "# nothing"
+    echo "#"
+    echo "##################################################################"
 # test routine
-if [ "$#" -ge 3 ] && [ "$3" = "-t" ]
+elif [ "$#" -eq 1 ] && [ "$1" = "-t" ]
 then
     touch "test.___"
     if test -f "test.___"
@@ -39,23 +81,6 @@ then
     else
         echo "nd_rm: ERROR: failed to create test file; verify permissions."
     fi
-elif [ "$#" -ge 3 ] && [ "$3" != "-t" ]
-then
-    echo "nd_ls: FAIL: unrecognized flag; test syntax is: sh nd_rm.sh ""filepathname.extension"" [""-i""] [""-t""]"
 else
-    # execute request
-    if "$#" -ge 1
-    then
-        echo "Deleting file(s): [$1]...";
-        if [ "$#" -ge 2 ] && [ "$2" = "-i" ]
-        then
-            echo "Using Switch(es): [$2]...";
-            rm -i "$1"
-        else
-            echo "Deleting file(s): [$1]...";
-            rm "$1"
-        fi
-    else
-        echo "nd_rm: ERROR: file pathname is missing; syntax is: sh nd_rm.sh ""filepathname.extension"" [""-i""]"
-    fi
+    echo "nd_ls: ERROR: unrecognized input; syntax is: sh nd_rm.sh -h"
 fi
